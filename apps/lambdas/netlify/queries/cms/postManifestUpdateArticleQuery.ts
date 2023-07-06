d42333fd9de2ca5573c26bb7461b14466ea61d9e
Dominik <bendominik@gmail.com>
2023-07-06 12:02:34 +0200

    trigger mesh deploy on railway app

--- apps/lambdas/netlify/queries/cms/postManifestUpdateArticleQuery.ts
+++ apps/lambdas/netlify/queries/cms/postManifestUpdateArticleQuery.ts
@@ -1,22 +1,22 @@
 export const postManifestUpdateArticleQuery = (title, description, oid) => {
   return `
-    mutation CreateManifestChangelogArticle {
-      strapi_createGithubManifestoChangelog(
-        data: {ManifestoArticle: {Title: "${title}", Description: "${description}", Version: "${oid}"}}
-      ) {
-        data {
-          id
-          attributes {
-            ManifestoArticle {
-              id
-              Description
-              UpdateDate
-              Version
-              Title
+      mutation CreateManifestChangelogArticle {
+        strapi_createGithubManifestoChangelog(
+          data: {ManifestoArticle: {Title: "${title}", Description: "${description}", Version: "${oid}"}}
+        ) {
+          data {
+            id
+            attributes {
+              ManifestoArticle {
+                id
+                Description
+                UpdateDate
+                Version
+                Title
+              }
             }
           }
         }
       }
-    }
     `;
 };
