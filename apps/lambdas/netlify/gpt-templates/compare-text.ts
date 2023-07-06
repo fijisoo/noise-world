1a98684a9bec9c33868fd55b9ab6b19d89413939
Dominik <bendominik@gmail.com>
2023-07-06 12:50:42 +0200

    gpt prompt template improvement

--- apps/lambdas/netlify/gpt-templates/compare-text.ts
+++ apps/lambdas/netlify/gpt-templates/compare-text.ts
@@ -5,6 +5,12 @@ This prompt is separated to two sections. First section is instruction what we w
 I would like to write blog article. The blog is about changes and more precise about changes in business paths, infrastucture, growth, visions about music industry. I would like to write this article by comparing texts. I have a two texts which are about the same content but in two different versions. The text is about documentation about project. It's technical documentation or should be more technical than any other blog text. The article should be readable by any nontechnical person and any nontechnical person should understand what exactly changed. The description should be about content so the business aspect not about meta data changes.   
 MAKE IT PRODUCTION READY. WRITE IT WITH MARKDOWN TAGS. MAX WORDS FOR WHOLE ARTICLE: 100. SEPARATE TITLE TO THE REST OF ARTICLE WITH "@@@@". DO NOT FORMAT THE TEXT.
 
+So the template would be:
+
+*TITLE_OF_ARTICLE*
+@@@@
+*REST_ARTICLE*
+
 #SECOND SECTION
 Text 1 below:
 ${oldText.toString()}
