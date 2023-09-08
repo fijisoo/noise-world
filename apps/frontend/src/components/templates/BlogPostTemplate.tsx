998884e4db060160bd21a2bfe657255e8b8cca7f
Dominik <bendominik@gmail.com>
2023-09-08 13:46:02 +0200

    blog post update

--- apps/frontend/src/components/templates/BlogPostTemplate.tsx
+++ apps/frontend/src/components/templates/BlogPostTemplate.tsx
@@ -1,3 +1,24 @@
+import Image from "next/image";
+
 export const BlogPostTemplate = ({ slug }: any) => {
-  return <div className="flex">Blog post {slug}</div>;
+  const { featuredImgSrc, title, content, publishDate, altText } = {
+    title: "Title of the post #0",
+    content:
+      "Description of the post Description of the postDescription of the postDescription of the postDescription of the postDescription of the postDescription of the postDescription of the post",
+    publishDate: "09.09.2023",
+    featuredImgSrc:
+      "https://www.ibisbis.com.au/adm/thumb2.aspx?src=/userfiles/Images/News/Work%20in%20Progress.jpg",
+    altText: "Post image #1",
+  };
+
+  return (
+    <div className="flex max-w-[600px] flex-col">
+      <div className="relative flex w-full w-[600px] h-[300px]">
+        <Image src={featuredImgSrc} alt={altText} fill unoptimized />
+      </div>
+      <div className="flex w-full justify-end text-xxs">{publishDate}</div>
+      <div className="flex w-full text-base font-bold">{title}</div>
+      <div className="flex w-full text-xs">{content}</div>
+    </div>
+  );
 };
