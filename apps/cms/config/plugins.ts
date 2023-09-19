2d01e6978a213721c01ce0d36fbabfb8d74df1ff
Dominik <bendominik@gmail.com>
2023-09-19 12:11:40 +0200

    cms cloudinary

--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -1,16 +1,30 @@
 export default {
   graphql: {
     config: {
-      endpoint: '/graphql',
+      endpoint: "/graphql",
       shadowCRUD: true,
-      playgroundAlways: process.env.STRAPI_PLAYGROUND === 'true',
+      playgroundAlways: process.env.STRAPI_PLAYGROUND === "true",
       depthLimit: 8,
       amountLimit: 100,
       apolloServer: {
         tracing: false,
-        introspection: process.env.STRAPI_PLAYGROUND === 'true',
+        introspection: process.env.STRAPI_PLAYGROUND === "true",
       },
       federation: true,
     },
-  }
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key: process.env.CLOUDINARY_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET,
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
 };
