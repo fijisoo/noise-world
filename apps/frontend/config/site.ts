a7e285358e097aa816003500f2acf19984f3c027
Dominik <bendominik@gmail.com>
2023-09-22 11:03:41 +0200

    og main image replace

--- apps/frontend/config/site.ts
+++ apps/frontend/config/site.ts
@@ -1,10 +1,10 @@
 export const site = {
     name: "syncArt",
-    title: "syncArt - keep your concert memories with you",
+    title: "syncArt - bridge your NFTs IRL",
     description:
-        "Scan qr code before the concert to receive a virtual stamp and access exclusives after the concert",
+        "Scan qr code and access your unique NFT",
     url: "https://sync.art",
-    ogImage: "https://sync.art/og.png",
+    ogImage: "https://sync.art/og-main.png",
     links: {
         github: "https://github.com/syncArt",
     },
