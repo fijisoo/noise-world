14903273c4118427e73bb971497d0a29bd910e4c
Dominik <bendominik@gmail.com>
2023-09-25 19:53:08 +0200

    update og images for main page

--- apps/frontend/src/utils/metadata.tsx
+++ apps/frontend/src/utils/metadata.tsx
@@ -1,35 +1,36 @@
-import {site} from "../../config/site";
+import { site } from "../../config/site";
 
 export const metadataShared = {
-    title: site.title,
+  title: site.title,
+  description: site.description,
+  icon: ["./icon.ico"],
+  keywords: [
+    "art",
+    "concert",
+    "web3",
+    "decentralization",
+    "festival",
+    "streaming services",
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://sync.art/og-syncart.png",
+    title: site.name,
     description: site.description,
-    icon: ["./icon.ico"],
-    keywords: [
-        "art",
-        "concert",
-        "web3",
-        "decentralization",
-        "festival",
-        "streaming services",
-    ],
-    openGraph: {
-        type: "website",
-        locale: "en_US",
-        url: "https://sync.art/og-syncart.png",
-        title: site.name,
-        description: site.description,
-        siteName: site.name,
-    },
-    twitter: {
-        card: "summary_large_image",
-        title: site.name,
-        description: site.description,
-        images: [site.ogImage],
-        creator: "@fijisoo",
-    },
-    icons: {
-        icon: "https://sync.art/icon.ico",
-        shortcut: "/icon.png",
-        apple: "/icon.png",
-    },
+    siteName: site.name,
+    images: ["https://sync.art/og-syncart.png"],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: site.name,
+    description: site.description,
+    images: [site.ogImage],
+    creator: "@fijisoo",
+  },
+  icons: {
+    icon: "https://sync.art/icon.ico",
+    shortcut: "/icon.png",
+    apple: "/icon.png",
+  },
 };
