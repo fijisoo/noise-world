2e6ad68a25c9949ee0bcb4668eccf0ecef573fd6
Dominik <bendominik@gmail.com>
2023-09-28 20:16:09 +0200

    change blog post dynamic to auto cache

--- apps/frontend/src/app/blog/[slug]/page.tsx
+++ apps/frontend/src/app/blog/[slug]/page.tsx
@@ -56,7 +56,7 @@ export async function generateMetadata({ params }: any) {
   };
 }
 
-export const dynamic = "force-dynamic";
+export const dynamic = "auto";
 
 export default function Page({ params: { slug } }: any) {
   return <BlogPostTemplate slug={slug} />;
